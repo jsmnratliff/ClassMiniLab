94aa0efbf824cde4f73168dfce3b16db0e7812a9
jsmnratliff <141177689+jsmnratliff@users.noreply.github.com>
2023-09-11 15:46:39 -0700

    firstDraft

---
+++ index.js
@@ -0,0 +1,15 @@
+class user {
+    constructor(email, username, password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getUsername() {
+        return this.username;
+    }
+    getPassword() {
+        return this.password;
+    }
